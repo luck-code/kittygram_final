a489a4aabe19e115df11df70f5674b083f976911
Maxim_Mazanko <iluckcode@gmail.com>
2026-05-01 05:38:14 +0300

    add main.yml

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://kittygram24.duckdns.org/', '158.160.170.223']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,10 +56,15 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
+
 
 
 # Password validation
@@ -111,3 +116,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
